b58a45ba0b30e0d8a97fb8dbfb5edf8085bb3cf1
Samir ram <samirram007@gmail.com>
2023-01-25 16:08:16 +0530

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,10 @@
+<template>
+    How To Install Vue 3 in Laravel 9 with Vite - TechvBlogs
+</template>
+<script lang="js">
+export default {
+    mounted() {
+        console.log('Example component mounted.')
+    }
+}
+</script>
